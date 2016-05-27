25f298f0cfaad099dfd08bce9a1811f8d7c8fe15
Avrumi Friedman <info@lakewoodprogramming.com>
2016-05-27 11:46:26 -0400

    Initial commit

---
+++ MvcApplication25/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication25.Models;
+
+namespace MvcApplication25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string text)
+        {
+            var viewModel = new IndexViewModel();
+            viewModel.Text = text;
+            if (!String.IsNullOrEmpty(text))
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                viewModel.LetterCount = GetWordCount(text);
+                sw.Stop();
+                viewModel.ListElapsed = sw.Elapsed;
+                sw.Reset();
+                sw.Start();
+                viewModel.LetterCountDictionary = GetWordCountDictionary(text);
+                sw.Stop();
+                viewModel.DictionaryElapsed = sw.Elapsed;
+            }
+            return View(viewModel);
+        }
+
+
+        private IEnumerable<LetterCount> GetWordCount(string text)
+        {
+            List<LetterCount> letterCounts = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Select(c => new LetterCount
+            {
+                Letter = c,
+                Count = 0
+            }).ToList();
+
+            foreach (char c in text.ToUpper())
+            {
+                foreach (LetterCount lc in letterCounts)
+                {
+                    if (lc.Letter == c)
+                    {
+                        lc.Count++;
+                    }
+                }
+            }
+
+            return letterCounts;
+        }
+
+        private Dictionary<char, int> GetWordCountDictionary(string text)
+        {
+            Dictionary<char, int> results = new Dictionary<char, int>();
+            foreach (char letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            {
+                results.Add(letter, 0);
+            }
+
+            foreach(char c in text.ToUpper())
+            {
+                if (results.ContainsKey(c))
+                {
+                    results[c]++;
+                }
+            }
+
+            return results;
+        } 
+    }
+}
---
+++ MvcApplication25/Models/IndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication25.Models
+{
+    public class IndexViewModel
+    {
+        public string Text { get; set; }
+        public IEnumerable<LetterCount> LetterCount { get; set; } 
+        public Dictionary<char, int> LetterCountDictionary { get; set; }
+        public TimeSpan ListElapsed { get; set; }
+        public TimeSpan DictionaryElapsed { get; set; }
+        
+    }
+
+    public class LetterCount
+    {
+        public char Letter { get; set; }
+        public int Count { get; set; }
+    }
+}
